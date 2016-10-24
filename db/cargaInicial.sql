fd7454164c6f48637848639c6f4d43d3ba8bf81f
cryptojew <zoharstartup@gmail.com>
2016-10-24 18:48:17 -0400

    Create cargaInicial.sql
    Submit inicial

---
+++ db/cargaInicial.sql
@@ -0,0 +1,50 @@
+-- 
+-- 
+--
+--
+
+INSERT INTO PESSOA(NOME, SOBRENOME, DATA_NASCIMENTO,
+             SG_SEXO, SG_IDIOMA_ISO639, DOCUMENTO, BIO)
+VALUES(      'Julio Cesar','Torres dos Santos',
+             '1976-11-08','M','PT','','')
+; 
+INSERT INTO PESSOA(NOME, SOBRENOME, DATA_NASCIMENTO,
+             SG_SEXO, SG_IDIOMA_ISO639, DOCUMENTO, BIO)
+VALUES(      'Ernst','Junger', NULL 
+             ,'M','DE','','')
+;
+INSERT INTO PESSOA(NOME, SOBRENOME, DATA_NASCIMENTO,
+             SG_SEXO, SG_IDIOMA_ISO639, DOCUMENTO, BIO)
+VALUES(      'Edgard','Allan Poe',
+             NULL,'M','EN','','')
+;
+INSERT INTO PESSOA(NOME, SOBRENOME, DATA_NASCIMENTO,
+             SG_SEXO, SG_IDIOMA_ISO639, DOCUMENTO, BIO)
+VALUES(      'Karl','May',
+             NULL,'M','DE','','')
+;
+INSERT INTO PESSOA(NOME, SOBRENOME, DATA_NASCIMENTO,
+             SG_SEXO, SG_IDIOMA_ISO639, DOCUMENTO, BIO)
+VALUES(      'Howard Phillips','Lovecraft',
+             NULL,'M','EN','','')
+;
+INSERT INTO PESSOA(NOME, SOBRENOME, DATA_NASCIMENTO,
+             SG_SEXO, SG_IDIOMA_ISO639, DOCUMENTO, BIO)
+VALUES(      'Rudyard','Kipling',
+             NULL,'M','EN','','')
+;
+INSERT INTO PESSOA(NOME, SOBRENOME, DATA_NASCIMENTO,
+             SG_SEXO, SG_IDIOMA_ISO639, DOCUMENTO, BIO)
+VALUES(      'Adolf','Hitler',
+             '1889-04-29','M','DE','','Née em Brannau-am-Inn')
+;
+INSERT INTO PESSOA(NOME, SOBRENOME, DATA_NASCIMENTO,
+             SG_SEXO, SG_IDIOMA_ISO639, DOCUMENTO, BIO)
+VALUES(      'Ian','Kershaw',
+             NULL,NULL,'EN','','')
+;
+INSERT INTO PESSOA(NOME, SOBRENOME, DATA_NASCIMENTO,
+             SG_SEXO, SG_IDIOMA_ISO639, DOCUMENTO, BIO)
+VALUES(      'Anthony','Beevor',
+             NULL,NULL,'EN','','')
+;
